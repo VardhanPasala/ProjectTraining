373b6e02fdf061a91370713a69904b50cbc9e8f4
VardhanPasala <pasala.Govardhan@cognizant.com>
2024-07-11 19:07:07 +0530

    Added Challenge 1 files

---
+++ Fundamentals_Part_1/Challenge_1/script.js
@@ -0,0 +1,10 @@
+const massMark=78;
+const heightMark=1.69;
+const massJohn=92;
+const heightJohn=1.95;
+
+markBMI=massMark/(heightMark*heightMark);
+johnBMI= massJohn/(heightJohn*heightJohn);
+const markHigherBMI=(markBMI>johnBMI)
+ 
+ console.log(markBMI,johnBMI,markHigherBMI);
